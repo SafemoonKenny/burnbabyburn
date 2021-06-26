239678dd554647e033b6d3b22f233d8b15b9ee2b
SafemoonKenny <86535071+SafemoonKenny@users.noreply.github.com>
2021-06-26 21:51:18 +0100

    Update BurnAnalysis.py

--- BurnAnalysis.py
+++ BurnAnalysis.py
@@ -2,7 +2,7 @@
 # Importing necessary packages
 from datetime import datetime, timedelta
 
-def burn_calculation(circulating=580.828875377658,target_mc=1, target_price=0.001,limit = 10000):
+def burn_calculation(circulating=580.828875377658,target_mc=1, target_price=0.001,limit = 10000,override=False):
 
     # Variables in Function
     # -- circulating: Circulating Supply of Safemoon
@@ -49,24 +49,24 @@ def burn_calculation(circulating=580.828875377658,target_mc=1, target_price=0.00
             break
 
 
-        # Daily Price and Volume Estimate
-        if counter <= 30*1.5: # Pre-Wallet Release
-            daily_price = initial_price # USD per SM
-            daily_volume = 20 # million USD
-        elif counter <= 30*6: # Wallet Released and Pre-Exchange
-            daily_price = 0.00001 # USD per SM 
-            daily_volume = 100 # million USD
-        elif counter <= 30*12: # Exchange Released 
-            daily_price = 0.00008 # USD per SM 
-            daily_volume = 500 # million USD
-        else: # Gambia Partnership Announced
-            daily_price = 0.0001 # USD per SM                    
-            daily_volume = 1000 # million USD
-
-            
-#        daily_price = 0.000002939
-#        daily_volume = 14.95
-        
+        # Daily Price and Volume
+        if override:
+            daily_price = 0.000002939
+            daily_volume = 14.95
+        else:
+            if counter <= 30*1.5: # Pre-Wallet Release
+                daily_price = initial_price # USD per SM
+                daily_volume = 20 # million USD
+            elif counter <= 30*6: # Wallet Released and Pre-Exchange
+                daily_price = 0.00001 # USD per SM 
+                daily_volume = 100 # million USD
+            elif counter <= 30*12: # Exchange Released 
+                daily_price = 0.00008 # USD per SM 
+                daily_volume = 500 # million USD
+            else: # Gambia Partnership Announced
+                daily_price = 0.0001 # USD per SM                    
+                daily_volume = 1000 # million USD
+      
         # Burn Reflection
         burned = (daily_volume*pow(10,6)) * (burn_wallet*pow(10,12)) / (20*pow(10,15)*daily_price) / pow(10,12) # trillion
 
@@ -122,8 +122,6 @@ def burn_calculation(circulating=580.828875377658,target_mc=1, target_price=0.00
         investment = 3500 # GBP
         average_buy = 0.0000035 # USD per SM
         print("Initial Investment:",investment)
-        print("Ending Value:",int(round(investment*daily_price/average_buy,0)))
-        print("Return:",round(pow(daily_price/average_buy,1/(int(current_date.strftime("%Y"))/365))-1,1)*100," %")
+        print("Ending Value:",int(round(investment*target_price/average_buy,0)))
+        print("CAGR:",round(pow(target_price/average_buy,1/(int(current_date.strftime("%Y"))/365))-1,0)*100,"%")
         
-        
-
